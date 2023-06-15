f545021ff1ed935a3d349cf447d016f54ec87bf2
Igor Vernyy <62211384+sheograph@users.noreply.github.com>
2023-06-15 11:54:43 +0300

    Add files via upload

---
+++ javascript/dropdown.js
@@ -0,0 +1,21 @@
+function showDrop(DrpID) {
+    document.getElementById(DrpID).classList.toggle("show");
+};
+
+function AddDropdownClickEvent() {
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
+
+        if (!event.target.matches('.dropbtn')) {
+
+            var dropdowns = document.getElementsByClassName("dropdown_content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    };
+};
